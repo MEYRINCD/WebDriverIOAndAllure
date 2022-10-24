dffffa5fb1c14ab73b6810b3032344898485f383
Vlad <113798755+MEYRINCD@users.noreply.github.com>
2022-10-25 00:06:39 +0300

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -123,7 +123,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['docker'],
+    services: ['docker','selenium-standalone']
     //selenium-standalone
     
     // Framework you want to run your specs with.
